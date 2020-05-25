12972e8b1eaa53998df40faf5342da0f35409809
Wojciech Lawren <wojciech.lawren@gmail.com>
2020-05-25 20:06:56 +0200

    numba

--- numba_str_test.py
+++ numba_str_test.py
@@ -1,37 +1,42 @@
 #! /usr/bin/env python
-import math
 import secrets
 from timeit import default_timer as time
 
 import numpy as np
 from numba import cuda
 
+E = 2000
+DIM = (40, 50)
+M = 2 ** 32
 
-@cuda.jit('void(i1[:,:], i1[:], i4[:])')
+
+def hash(s):  # <=>
+    h = 0
+
+    for i in bytearray(s, 'utf-8'):
+        h = (i + (h << 6) + (h << 16) - h) % M
+
+    return np.uint32(h)
+
+
+@cuda.jit('void(u4[:], u4, i4[:])')
 def str_kernel(arr, s, res):
     pos = cuda.grid(1)
-    size = arr[pos].size
-    if size != s.size:
-        return
-    for i in range(size):
-        if arr[pos][i] != s[i]:
-            return
-    cuda.atomic.compare_and_swap(res, 0, pos)
+    if pos < E:
+        if arr[pos] == s:
+            cuda.atomic.compare_and_swap(res, 0, pos)
 
 
-arr = [secrets.token_hex() for _ in range(2048)]
-np_arr = np.array(list(map(lambda x: np.byte(bytearray(x, 'utf-8')), arr)))
-
-threadsperblock = min(1024, np_arr.shape[0])
-blockspergrid = min(1024, math.ceil(np_arr.shape[0] / threadsperblock))
-assert (threadsperblock * blockspergrid == np_arr.shape[0])
+arr = [secrets.token_hex() for _ in range(E)]
+np_arr = np.array([hash(x) for x in arr])
+assert (DIM[0] * DIM[1] == np_arr.shape[0])
+assert (np_arr.dtype == np.uint32)
 
 stream = cuda.stream()
 dA = cuda.to_device(np_arr, stream)
-dB = cuda.device_array(len(np_arr[0]), np_arr.dtype, stream=stream)
 
 st = arr[secrets.randbelow(len(arr))]
-np_st = np.byte(bytearray(st, 'utf-8'))
+h_st = hash(st)
 
 np_res = np.zeros(1, dtype=np.int32)
 dC = cuda.to_device(np_res, stream)
@@ -39,11 +44,10 @@ dC = cuda.to_device(np_res, stream)
 s = time()
 
 with stream.auto_synchronize():
-    cuda.to_device(np_st, stream, to=dB)
-    str_kernel[blockspergrid, threadsperblock](dA, dB, dC)
+    str_kernel[DIM[0], DIM[1]](dA, h_st, dC)
     dC.to_host(stream)
 
 e = time()
 
 print('cuda: {0:1.6f}'.format(e - s))
-print('{0} == {1}'.format(arr[np_res[0]], st))
+print('{0} = {1}'.format(arr[np_res[0]], st))

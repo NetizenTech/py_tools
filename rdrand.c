076210a2260b8e0b03a65b2d6f0fe118ad4bcf68
Wojciech Lawren <wojciech.lawren@gmail.com>
2021-01-29 19:39:00 +0100

    rdrand/rdseed 16|32|64

--- rdrand.c
+++ rdrand.c
@@ -1,35 +1,127 @@
+// lscpu | egrep -i --color=auto "rdrand|rdseed|cmov"
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-static PyObject *method_rand64(PyObject *self)
-{
-    unsigned long v;
+// https://software.intel.com/content/www/us/en/develop/articles/intel-digital-random-number-generator-drng-software-implementation-guide.html
+#define LOOP 10
 
-    asm volatile ( "movl $10, %%ecx\n\t"
-                   ".rd_start:\n\t"
-                       "rdrand %0\n\t"
-                       "jc .rd_end\n\t"
-                       "loop .rd_start\n\t"
-                       "cmovnc %%rcx, %0\n\t"
-                   ".rd_end:"
-                   :"=r"(v) : : "rcx", "cc" );
+static PyObject *method_rand64(PyObject *self) {
+  unsigned long v;
 
-    return PyLong_FromUnsignedLong(v);
+  asm volatile("movl %1, %%ecx\n\t"
+               "1:\n\t"
+               "rdrand %0\n\t"
+               "jc 2f\n\t"
+               "loop 1b\n\t"
+               "cmovnc %%rcx, %0\n\t"
+               "2:"
+               : "=r"(v)
+               : "n"(LOOP)
+               : "rcx", "cc");
+
+  return PyLong_FromUnsignedLong(v);
 }
 
+static PyObject *method_rand32(PyObject *self) {
+  unsigned v;
+
+  asm volatile("movl %1, %%ecx\n\t"
+               "1:\n\t"
+               "rdrand %0\n\t"
+               "jc 2f\n\t"
+               "loop 1b\n\t"
+               "cmovnc %%ecx, %0\n\t"
+               "2:"
+               : "=r"(v)
+               : "n"(LOOP)
+               : "rcx", "cc");
+
+  return PyLong_FromUnsignedLong((unsigned long)v);
+}
+
+static PyObject *method_rand16(PyObject *self) {
+  unsigned short v;
+
+  asm volatile("movl %1, %%ecx\n\t"
+               "1:\n\t"
+               "rdrand %0\n\t"
+               "jc 2f\n\t"
+               "loop 1b\n\t"
+               "cmovnc %%cx, %0\n\t"
+               "2:"
+               : "=r"(v)
+               : "n"(LOOP)
+               : "rcx", "cc");
+
+  return PyLong_FromUnsignedLong((unsigned long)v);
+}
+
+static PyObject *method_seed64(PyObject *self) {
+  unsigned long v;
+
+  asm volatile("movl %1, %%ecx\n\t"
+               "1:\n\t"
+               "rdseed %0\n\t"
+               "jc 2f\n\t"
+               "loop 1b\n\t"
+               "cmovnc %%rcx, %0\n\t"
+               "2:"
+               : "=r"(v)
+               : "n"(LOOP)
+               : "rcx", "cc");
+
+  return PyLong_FromUnsignedLong(v);
+}
+
+static PyObject *method_seed32(PyObject *self) {
+  unsigned v;
+
+  asm volatile("movl %1, %%ecx\n\t"
+               "1:\n\t"
+               "rdseed %0\n\t"
+               "jc 2f\n\t"
+               "loop 1b\n\t"
+               "cmovnc %%ecx, %0\n\t"
+               "2:"
+               : "=r"(v)
+               : "n"(LOOP)
+               : "rcx", "cc");
+
+  return PyLong_FromUnsignedLong((unsigned long)v);
+}
+
+static PyObject *method_seed16(PyObject *self) {
+  unsigned short v;
+
+  asm volatile("movl %1, %%ecx\n\t"
+               "1:\n\t"
+               "rdseed %0\n\t"
+               "jc 2f\n\t"
+               "loop 1b\n\t"
+               "cmovnc %%cx, %0\n\t"
+               "2:"
+               : "=r"(v)
+               : "n"(LOOP)
+               : "rcx", "cc");
+
+  return PyLong_FromUnsignedLong((unsigned long)v);
+}
+/* Return value:
+    - appropriate random number
+    - 0    -> if rdrand fails
+    - NULL -> if PyLong_FromUnsignedLong fails
+*/
 static PyMethodDef RdrandMethods[] = {
-    {"rand64", (PyCFunction)method_rand64, METH_NOARGS, "Python interface for rdrand"},
-    {NULL, NULL, 0, NULL}
-};
+    {"rand64", (PyCFunction)method_rand64, METH_NOARGS, "rdrand 64 bit RNG"},
+    {"rand32", (PyCFunction)method_rand32, METH_NOARGS, "rdrand 32 bit RNG"},
+    {"rand16", (PyCFunction)method_rand16, METH_NOARGS, "rdrand 16 bit RNG"},
+    {"seed64", (PyCFunction)method_seed64, METH_NOARGS, "rdseed 64 bit seed"},
+    {"seed32", (PyCFunction)method_seed32, METH_NOARGS, "rdseed 32 bit seed"},
+    {"seed16", (PyCFunction)method_seed16, METH_NOARGS, "rdseed 16 bit seed"},
+    {NULL, NULL, 0, NULL}};
 
-static struct PyModuleDef rdrandmodule = {
-    PyModuleDef_HEAD_INIT,
-    "rdrand",
-    "Python interface for rdrand",
-    -1,
-    RdrandMethods
-};
+static struct PyModuleDef rdrandmodule = {PyModuleDef_HEAD_INIT, "rdrand",
+                                          "Python interface for rdrand", -1,
+                                          RdrandMethods};
 
-PyMODINIT_FUNC PyInit_rdrand(void)
-{
-    return PyModule_Create(&rdrandmodule);
-}
+PyMODINIT_FUNC PyInit_rdrand(void) { return PyModule_Create(&rdrandmodule); }
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ def main():
     setup(name="rdrand",
           version="0.1.0",
           description="Python interface for rdrand",
+          long_description="https://www.amd.com/system/files/TechDocs/amd-random-number-generator.pdf",
           author="Wojciech Lawren",
           author_email="wojciech[at]lawren.eu",
           license="LGPL v3",

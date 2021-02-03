d8696a6a7135f67560976c16bd49266576add716
Wojciech Lawren <wojciech.lawren@gmail.com>
2021-02-03 11:39:23 +0100

    rdrand/rdseed 16|32|64

--- rdrand.c
+++ rdrand.c
@@ -1,109 +1,60 @@
-// lscpu | egrep -i --color=auto "rdrand|rdseed|cmov"
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-// https://software.intel.com/content/www/us/en/develop/articles/intel-digital-random-number-generator-drng-software-implementation-guide.html
 #define LOOP 10
 
+#define RAND "rdrand %0\n\t"
+#define SEED "rdseed %0\n\t"
+
+#define RD(i)                                                                  \
+  asm volatile("mov %1, %%ecx\n\t"                                             \
+               "1:\n\t" i "jc 2f\n\t"                                          \
+               "loop 1b\n\t"                                                   \
+               "xor %0, %0\n\t"                                                \
+               "2:"                                                            \
+               : "=r"(v)                                                       \
+               : "n"(LOOP)                                                     \
+               : "rcx", "cc")
+
 static PyObject *method_rand64(PyObject *self) {
-  unsigned long v;
+  register unsigned long v asm("rdi");
 
-  asm volatile("movl %1, %%ecx\n\t"
-               "1:\n\t"
-               "rdrand %0\n\t"
-               "jc 2f\n\t"
-               "loop 1b\n\t"
-               "cmovnc %%rcx, %0\n\t"
-               "2:"
-               : "=r"(v)
-               : "n"(LOOP)
-               : "rcx", "cc");
-
+  RD(RAND);
   return PyLong_FromUnsignedLong(v);
 }
 
 static PyObject *method_rand32(PyObject *self) {
-  unsigned v;
+  register unsigned v asm("edi");
 
-  asm volatile("movl %1, %%ecx\n\t"
-               "1:\n\t"
-               "rdrand %0\n\t"
-               "jc 2f\n\t"
-               "loop 1b\n\t"
-               "cmovnc %%ecx, %0\n\t"
-               "2:"
-               : "=r"(v)
-               : "n"(LOOP)
-               : "rcx", "cc");
-
+  RD(RAND);
   return PyLong_FromUnsignedLong((unsigned long)v);
 }
 
 static PyObject *method_rand16(PyObject *self) {
-  unsigned short v;
+  register unsigned short v asm("di");
 
-  asm volatile("movl %1, %%ecx\n\t"
-               "1:\n\t"
-               "rdrand %0\n\t"
-               "jc 2f\n\t"
-               "loop 1b\n\t"
-               "cmovnc %%cx, %0\n\t"
-               "2:"
-               : "=r"(v)
-               : "n"(LOOP)
-               : "rcx", "cc");
-
+  RD(RAND);
   return PyLong_FromUnsignedLong((unsigned long)v);
 }
 
 static PyObject *method_seed64(PyObject *self) {
-  unsigned long v;
+  register unsigned long v asm("rdi");
 
-  asm volatile("movl %1, %%ecx\n\t"
-               "1:\n\t"
-               "rdseed %0\n\t"
-               "jc 2f\n\t"
-               "loop 1b\n\t"
-               "cmovnc %%rcx, %0\n\t"
-               "2:"
-               : "=r"(v)
-               : "n"(LOOP)
-               : "rcx", "cc");
-
+  RD(SEED);
   return PyLong_FromUnsignedLong(v);
 }
 
 static PyObject *method_seed32(PyObject *self) {
-  unsigned v;
+  register unsigned v asm("edi");
 
-  asm volatile("movl %1, %%ecx\n\t"
-               "1:\n\t"
-               "rdseed %0\n\t"
-               "jc 2f\n\t"
-               "loop 1b\n\t"
-               "cmovnc %%ecx, %0\n\t"
-               "2:"
-               : "=r"(v)
-               : "n"(LOOP)
-               : "rcx", "cc");
-
+  RD(SEED);
   return PyLong_FromUnsignedLong((unsigned long)v);
 }
 
 static PyObject *method_seed16(PyObject *self) {
-  unsigned short v;
+  register unsigned short v asm("di");
 
-  asm volatile("movl %1, %%ecx\n\t"
-               "1:\n\t"
-               "rdseed %0\n\t"
-               "jc 2f\n\t"
-               "loop 1b\n\t"
-               "cmovnc %%cx, %0\n\t"
-               "2:"
-               : "=r"(v)
-               : "n"(LOOP)
-               : "rcx", "cc");
-
+  RD(SEED);
   return PyLong_FromUnsignedLong((unsigned long)v);
 }
 /* Return value:
